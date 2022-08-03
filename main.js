03ee21a9588bf88d75cde1bc6f97eb7c18f19fe2
Raghul <raghulnov13@gmail.com>
2022-08-03 16:59:48 +0530

    files

---
+++ main.js
@@ -0,0 +1,41 @@
+// Addition
+let a = 10;
+let b = 30;
+let c = a + b;
+console.log(c);
+
+// Subtraction
+let x = 10;
+let y = 40;
+let z = x - y;
+let j = y - x;
+
+console.log(z);
+console.log(j);
+typeof j;
+
+// 
+let d = 1 + "1";
+console.log(d);
+typeof d;
+
+// 
+var e = 20;
+var f = 5;
+var g = e/f;
+console.log(g);
+
+// 
+let h = 7;
+
+let i = ++h;
+console.log(i);
+
+// 
+for (let k = 0; k < 10; k++) {
+    console.log(k);
+}
+// 
+let age = 50;
+age > 50;
+age < 50;
