d60730464893abcb1c11cb97da77ea30f45f3273
Raghul <raghulnov13@gmail.com>
2022-08-06 16:39:00 +0530

    files

---
+++ array.js
@@ -0,0 +1,161 @@
+const list = [
+    { name: "Briyani", price: 120, },
+    { name: "Dosa", price: 40 },
+    { name: "Idly", price: 10 },
+    { name: "Pongal", price: 30 },
+    { name: "Chicken", price: 200 },
+    { name: "Mutton", price: 300 },
+    { name: "Meals", price: 180 },
+];
+
+
+// filter method output comes in array inside with object [{...} {...}]
+const filteredElements = list.filter((list) => {
+    return list.price <= 100;
+}
+);
+
+// console.log(filteredElements);
+
+// -----------------------
+
+// Map method used to display separate name or price list
+const listNames = list.map((list) => {
+    return list.name;
+}
+);
+
+console.log(listNames);
+
+// ------------------------------
+
+// find method used to find single object in array
+// output comes in
+
+let findlist = list.find((list) => {
+    return list.name === "Mutton";
+}
+)
+console.log(findlist);
+
+// ------------------
+// foreach used to change element values
+
+list.forEach((list) => {
+    console.log(list.price * 1.5);
+}
+);
+
+// ---------------
+// some return true or false
+
+const hasExpensiveLists = list.some(list => {
+    return list.price <= 5
+}
+);
+console.log(hasExpensiveLists)
+
+// --------------
+// every return true or false if all elements satisty the content it will give true
+
+const hasExpensiveListsEvery = list.every(list => {
+    return list.price <= 1000
+}
+);
+console.log(hasExpensiveListsEvery)
+
+// ---------------
+
+// reduce used to 
+const total = list.reduce((currentTotal, list) => {
+    return list.price + currentTotal;
+
+}, 10);
+console.log(total);
+
+// ---------------------------------
+const items = [
+    { id: 1, name: "apple mobile", cost: 70000 },
+    { id: 2, name: "oneplus", cost: 40000 },
+    { id: 3, name: "vivo", cost: 30000 },
+    { id: 4, name: "oppo", cost: 28000 },
+]
+
+// filter
+let ral = items.filter(function (value) {
+    return value.cost < 60000;
+}
+
+);
+
+// console.log( ral);
+
+// find
+let raghul = items.find(function (value) {
+    return value.cost < 40000;
+}
+
+);
+
+console.log(raghul);
+
+// findIndex
+
+let ra = items.findIndex(function (value) {
+    return value.cost < 40000;
+}
+
+);
+
+console.log(ra);
+
+
+// map we can change own behaviour values
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+
+let final = numbers.map(function (val) {
+    return val * 10;
+});
+console.log(final);
+
+// example using reference
+const people = [
+    { id: 1, first_name: "Raghul", last_name: "raj" },
+    { id: 2, first_name: "Virat", last_name: "kohli" },
+    { id: 3, first_name: "Ms", last_name: "dhoni" },
+    { id: 4, first_name: "David", last_name: "warner" },
+
+];
+
+let detail = people.map(function (ans) {
+    return ans.first_name + ans.last_name;
+});
+console.log(detail);
+
+// return values in array
+let name = people.map(function (info) {
+    return [info.first_name, info.last_name].join(" ");
+});
+console.log(name);
+//  map will also give boolean result for all array
+//diff btwn map and reduce... reduce will give single values
+
+// reduce
+const donations = [100, 200, 50, 325, 150, 3000]
+
+// donations.reduce (function(previousValue, currentValue){
+
+//     console.log("previousValue", previousValue);
+//     console.log("currentValue", currentValue);
+
+//     return previousValue + currentValue;
+
+// });
+
+// for total value
+let totalDonations = donations.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+});
+
+console.log("totalDonations", totalDonations);
---
+++ fun.js
@@ -0,0 +1,23 @@
+function ral (civil){
+    if (civil > 100){
+        return 'good';
+    }
+    else{
+        return 'ok';
+
+    }
+    
+}
+
+console.log(ral (101));
+
+// array
+var marks =[];
+marks.push (20);
+marks.push (30);
+marks.push (40);
+marks.push (50);
+marks.push (60);
+marks.push (70);
+
+console.log(marks);
