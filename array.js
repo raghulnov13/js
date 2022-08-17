274727fb566a0dc885161e80c8c7fd24536e7f8f
Raghul <raghulnov13@gmail.com>
2022-08-17 14:46:47 +0530

    file

--- array.js
+++ array.js
@@ -181,3 +181,19 @@ else{
     if (x > 0)
     console.log ("positive");
     }
+
+
+
+
+// Date
+const d = new Date();
+console.log(d.getTime());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(Date.now());
+
