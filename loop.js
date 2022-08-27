4bb578e0385f70effe97d0fa3dd0f25a39430f56
Raghul <raghulnov13@gmail.com>
2022-08-27 12:06:59 +0530

    file

--- loop.js
+++ loop.js
@@ -52,11 +52,51 @@ for (let i = 1; i <= 15; i++) {
 //     i--;
 // }
 // 
-let i = 5;
-while (i <= 10) {
-    if (i % 2 == 0) {
-        console.log(i);
+// let i = 5;
+// while (i <= 10) {
+//     if (i % 2 == 0) {
+//         console.log(i);
 
+//     }
+//     i++;
+// }
+
+
+// Do while
+let i = 11;
+do {
+    if (i % 2 !== 0) {
+        console.log(i);
     }
     i++;
+} while (i <= 10);
+
+
+// for-in object
+// call keys by . or by []
+const person = {
+    name: "raghul",
+    age: 25,
+    gender: "male"
+};
+for (let key in person) {
+    console.log(key + ":", person[key]);
+}
+
+// for-in array index position will display
+let mobiles = ["vivo", "oppo", "oneplus"];
+
+for (let key in mobiles) {
+    console.log(key);
+}
+
+// for-in array 
+let colors = ["red", "green", "yellow"];
+for(let key in colors){
+    console.log(colors[key]);
+}
+
+// for-of array gives value
+for (let mobile of mobiles) {
+    console.log("color: " + mobile);
 }
