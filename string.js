d8da52ebb88c587314c673ef4723e4441ceedbfe
Raghul <raghulnov13@gmail.com>
2022-09-03 09:07:34 +0530

    file

---
+++ string.js
@@ -0,0 +1,13 @@
+// String Property and Methods
+var a = "Welcome to the new world";
+var b = " Start the journey ";
+
+
+// charAt
+console.log(a.charAt(6));
+
+// charCodeAt
+console.log(a.charCodeAt(7));
+
+// Concat
+console.log(a.concat(b));
